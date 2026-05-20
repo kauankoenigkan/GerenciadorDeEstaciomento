163d1597c1a842f960423631d5d4f044faa843df
Kauan Koenigkan <281706847+kauankoenigkan@users.noreply.github.com>
2026-05-19 22:47:06 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeEstaciomento/Estacionamento.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaDeEstaciomento
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;                  
+        private List <string> Veiculos = new List <string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void CadastrarVeiculo()
+        {
+            Console.WriteLine("Digite sua placa para estacionar o carro..: ");
+            string placaCadastrar = Console.ReadLine().ToUpper();
+
+            if (Veiculos.Contains(placaCadastrar))
+            {
+                Console.WriteLine("O veículo já está estacionado aqui!");
+            }
+            else
+            {
+                Veiculos.Add(placaCadastrar);
+                Console.WriteLine($"Veículo com a placa: '{placaCadastrar}' foi cadastrado com sucesso!");
+            }
+        }
+
+        public void RemoverVeiculo() // Remover veículo e mostrar o valor da permanência no estacionamento.
+        {
+            Console.WriteLine("Digite a placa do veículo para remove-lo..: ");
+            string placaRemocao = Console.ReadLine().ToUpper();
+
+            if (Veiculos.Contains(placaRemocao))
+            {
+                int horas = 0;
+                decimal valorTotal = 0;
+
+                Console.WriteLine("Digite as horas que o veículo ficou estacionado..: ");
+                horas = Convert.ToInt32(Console.ReadLine());
+
+                valorTotal = precoInicial + (precoHora * horas);
+                Veiculos.Remove(placaRemocao);
+
+                Console.WriteLine();
+                Console.WriteLine($"O veículo com a placa: '{placaRemocao}' foi removido e o preço total ficou: R$ {valorTotal} .");
+            }
+            else
+            {
+                Console.WriteLine("Placa de veículo não encontrada nesse estacionamento!");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (Veiculos.Count == 0)
+            {
+                Console.WriteLine("O estacionamento está vazio!");
+            }
+            else
+            {
+                for (int i = 0; i < Veiculos.Count; i++)
+                {
+                    Console.WriteLine($" Placa de Veículo {i + 1}: {Veiculos[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ SistemaDeEstaciomento/Program.cs
@@ -0,0 +1,63 @@
+﻿// Neste Projeto de LAB você será desafiado a construir um sistema para um estacionamento,-
+// que será usado para gerenciar os veículos estacionados e realizar suas operações, -
+// como por exemplo adicionar um veículo, remover um veículo (e exibir o valor cobrado durante o período) e listar os veículos.
+
+using SistemaDeEstaciomento;
+
+decimal precoInicial = 0;
+decimal precoHora = 0;
+bool exibirMenu = true;
+
+Console.WriteLine("Digite o valor inicial do estacionamento..: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor por hora do estacionamento..: ");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento est = new Estacionamento(precoInicial, precoHora);
+
+Console.WriteLine("Bem-vindo(a) ao estacionamento KN!");
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Bem-vindo(a) ao estacionamento KN!");
+    Console.WriteLine();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.Clear();
+            est.CadastrarVeiculo();
+            break;
+
+        case "2":
+            Console.Clear();
+            est.RemoverVeiculo();
+            break;
+
+        case "3":
+            Console.Clear();
+            est.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Comando não encontrado...");
+            break;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pressione qualquer tecla para sair... ");
+    Console.ReadKey();
+}
+Console.Clear();
+Console.WriteLine("O programa se encerrou.");
